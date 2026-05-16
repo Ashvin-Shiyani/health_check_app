0097dd1ee18f956d3c3338e21e5007b265edb2e2
Ashvin Shiyani <ashvinshiyani10@icloud.com>
2026-05-15 21:45:59 -0300

    initial commit

---
+++ settings.py
@@ -0,0 +1,3 @@
+class Settings():
+    def __init__(self):
+        self.screen_bg_color = "#16213e"
---
+++ test.py
@@ -0,0 +1,12 @@
+sum = 0
+
+
+def get():
+    age = 25
+    weight = 61
+    height = 172
+    return age, weight, height
+
+
+age, weight, height = get()
+print(age, weight, height)
